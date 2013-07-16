b2ef17504d7dfd7cfe652c7097e4679df3fd3386
Ian Dees <ian.dees@gmail.com>
2013-07-15 21:48:26 -0500

    Initial commit of plugin based on walking papers plugin.

---
+++ src/org/openstreetmap/josm/plugins/fieldpapers/FieldPapersPlugin.java
@@ -0,0 +1,33 @@
+package org.openstreetmap.josm.plugins.fieldpapers;
+
+import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
+import static org.openstreetmap.josm.tools.I18n.marktr;
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * Main class for the walking papers plugin.
+ *
+ * @author Frederik Ramm <frederik@remote.org>
+ *
+ */
+public class FieldPapersPlugin extends Plugin
+{
+    static JMenu walkingPapersMenu;
+
+    public FieldPapersPlugin(PluginInformation info)
+    {
+        super(info);
+        MainMenu menu = Main.main.menu;
+        walkingPapersMenu = menu.addMenu(marktr("Walking Papers"), KeyEvent.VK_K, menu.defaultMenuPos, ht("/Plugin/WalkingPapers"));
+        walkingPapersMenu.add(new JMenuItem(new FieldPapersAddLayerAction()));
+    }
+}
