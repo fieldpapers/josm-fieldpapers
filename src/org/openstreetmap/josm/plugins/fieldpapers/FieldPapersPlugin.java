8f04e1cb51e3fb4aec1b0531a62e713f6c3f7181
Florian Schäfer <florian@schaeferban.de>
2018-02-19 22:21:36 +0100

    Move menu bar entry to imagery menu
    Instead of being at "Field Papers › Scanned map ...", the entry can now be found at "Imagery › FieldPapers snapshot".

--- src/org/openstreetmap/josm/plugins/fieldpapers/FieldPapersPlugin.java
+++ src/org/openstreetmap/josm/plugins/fieldpapers/FieldPapersPlugin.java
@@ -1,15 +1,8 @@
 package org.openstreetmap.josm.plugins.fieldpapers;
 
-import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
-import static org.openstreetmap.josm.tools.I18n.tr;
-
-import java.awt.event.KeyEvent;
-
-import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import org.openstreetmap.josm.gui.MainApplication;
-import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
@@ -20,14 +13,9 @@ import org.openstreetmap.josm.plugins.PluginInformation;
  * @author Frederik Ramm <frederik@remote.org>
  *
  */
-public class FieldPapersPlugin extends Plugin
-{
-    public FieldPapersPlugin(PluginInformation info)
-    {
+public class FieldPapersPlugin extends Plugin {
+    public FieldPapersPlugin(PluginInformation info) {
         super(info);
-
-        MainMenu menu = MainApplication.getMenu();
-        JMenu fpMenu = menu.addMenu("Field Papers", tr("Field Papers"), KeyEvent.VK_K, menu.getDefaultMenuPos(), ht("/Plugin/FieldPapers"));
-        fpMenu.add(new JMenuItem(new FieldPapersAddLayerAction()));
+        MainApplication.getMenu().imageryMenu.add(new JMenuItem(new FieldPapersAddLayerAction()));
     }
 }
